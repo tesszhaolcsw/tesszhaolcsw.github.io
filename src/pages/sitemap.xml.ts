c482282511669ecffe568e50fe545e4c36e22bd2
Mind X Associates <149201111+mindxassociates@users.noreply.github.com>
2026-09-19 12:02:17 -0500

    Add self-guided tools pages to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,7 +1,7 @@
 import { getCollection } from 'astro:content';
 export async function GET() {
   const site='https://www.tesszhaolcsw.com';
-  const fixed=['/','/about.html','/psychotherapy.html','/insurance-and-fees.html','/psychotherapy-cn.html','/insurance-cn.html','/privacy-policy.html','/hippa.html','/blog/','/zh/blog/'];
+  const fixed=['/','/about.html','/psychotherapy.html','/insurance-and-fees.html','/psychotherapy-cn.html','/insurance-cn.html','/privacy-policy.html','/hippa.html','/resources/','/zh/resources/','/blog/','/zh/blog/'];
   const en=(await getCollection('blogEn',({data})=>!data.draft)).map(p=>`/blog/${p.id.replace(/\.(md|mdx)$/,'')}/`);
   const zh=(await getCollection('blogZh',({data})=>!data.draft)).map(p=>`/zh/blog/${p.id.replace(/\.(md|mdx)$/,'')}/`);
   const urls=[...fixed,...en,...zh].map(path=>`<url><loc>${site}${path}</loc></url>`).join('');
